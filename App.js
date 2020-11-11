739b6f45dd6566ed41e1d0d04ecc381d3ffff7e7
Thiago Goulart <thiagogoulart1995@gmail.com>
2020-11-11 02:33:07 -0300

    1

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+
+  <KeyboardAvoidingView style={styles.containerLogin}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <TextInput style={styles.input}                    // Campo do email
+        placeholder="Email"
+        autoCorrect={false}
+        onChangeText={()=>{}}/>
+
+      <TextInput style={styles.input}                    // Campo da senha
+        placeholder="Senha"
+        autoCorrect={false}
+        onChangeText={()=>{}}/>
+
+      <TouchableOpacity style={styles.btnAcessar}>
+        <Text>Acessar</Text>
+      </TouchableOpacity>
+ 
+    </View>     
+  </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerLogin: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+  },
+  input:{
+    backgroundColor: '#FFF',
+    width: '90%',
+    marginBottom:15,
+    color:'#222',
+    fontSize: 17,
+    borderRadius: 7,
+    padding: 10,
+  },
+  btnAcessar:{
+    backgroundColor: '#35AAFF',
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
+  }
 });
