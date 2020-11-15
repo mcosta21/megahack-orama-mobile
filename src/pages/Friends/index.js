2d99568b02f229ae2700d4a37a71465cfda4f402
Victor Ladeira <victorladeirag@gmail.com>
2020-11-15 13:05:00 -0300

    create getFriends requisition

--- src/pages/Friends/index.js
+++ src/pages/Friends/index.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import styles from './styles';
 import {  Text, View, FlatList, TouchableOpacity } from 'react-native';
-import InputText from '../../components/InputText';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { LinearGradient } from 'expo-linear-gradient';
+import { AntDesign  } from '@expo/vector-icons';
+
+import TitleWelcome from '../../components/TitleWelcome';
 
 import api from '../../services/api';
 
@@ -13,22 +15,46 @@ export default function Friends(){
 
   const [ friends, setFriends ] = useState([]);
 
-  function handleSearchUser() {
-
+  function handleNavigateToHome() {
+    navigate('Home');
   }
 
+  useEffect(() => {
+    api.get('/friends').then(response => {
+      const { status, data } = response;
+
+      if(status === 200) {
+        setFriends(data);
+      }
+    });
+  }, []);
+
   return (
-    <View style={styles.searchContainer}>
-      <SafeAreaView>
-        <Text>Amigos</Text>
-        
-      </SafeAreaView>
-        <TouchableOpacity onPress={handleSearchUser} style={styles.searchBtn} >
-          <Text>Buscar</Text>
-        </TouchableOpacity>
-      <View style={styles.resultContainer}>
-        
+    <View style={styles.container}>
+
+      <View style={styles.header}>
+        <AntDesign onPress={ handleNavigateToHome } name="arrowleft" size={24} color="#FFF"/>
+        <Text style={styles.title}>ÓRAMA</Text>
+        <AntDesign name="arrowleft" size={24} color="#182024"/>
       </View>
+
+      <SafeAreaView>
+        <TitleWelcome title='Amigos' description='Estes são seus amigos investidores'/>
+      </SafeAreaView>
+
+      <LinearGradient
+        colors={['#24AC6E', '#34F683']}
+        start={{ x: 1, y: 1 }}
+        end={{ x: 0, y: 0 }}
+        style={styles.backgroundImage}
+      >
+        <View style={styles.resultContainer}>
+          {friends?.map((friend, key) => (
+            <Text key={key}>{friend.firstName}</Text>
+          ))}
+        </View>
+
+      </LinearGradient>
     </View>
   );
 };
--- src/pages/Friends/styles.js
+++ src/pages/Friends/styles.js
@@ -1,13 +1,33 @@
 import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
 
 export default StyleSheet.create({
-    searchContainer:{
-        flex: 1,
-        backgroundColor: '#182024',
-    },
-    resultContainer:{
-        flex: 1,
-        backgroundColor: '#182024',
-    },
+  container:{
+    flex: 1,
+    paddingHorizontal: 24,
+    backgroundColor: '#182024',
+    paddingTop: Constants.statusBarHeight + 20,
+    flexDirection: "column"
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 22,
+    color: '#FFF',
+    fontFamily: 'Ubuntu_700Bold'
+  },
+  resultContainer:{
+    flex: 1,
+    backgroundColor: '#182024',
+  },
 
+  backgroundImage: {
+    marginTop: 20,
+    width: '100%',
+    height: 60,
+    borderRadius: 8,
+  }
 });
